e2eeda5c7d64db69b387c306e132c75cd2031feb
vishalchandra736 <vishalchandra736@gmail.com>
2022-12-23 18:30:48 +0530

    Calculate Employee Wage using Switch Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,17 +2,24 @@ package com.blz.employeewagecomputation;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        int IS_Full_Time = 1;
-        int IS_PART_TIME = 2;
+//        int IS_FULL_TIME = 1;
+//        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHour, empWage;
         int empCheck = (int)(Math.floor(Math.random() * 10 % 3));
-        if (empCheck == IS_Full_Time)
-            empHour = 8;
-        else if (empCheck == IS_PART_TIME)
-            empHour = 4;
-        else
-            empHour = 0;
+        switch (empCheck) {
+            case 1 :
+                System.out.println("Employee was work  Full-Time");
+                empHour = 8;
+                break;
+            case 2 :
+                System.out.println("Employee was work Part-Time");
+                empHour = 4;
+                break;
+            default :
+                System.out.println("Employee was Absent");
+                empHour = 0;
+        }
         empWage = EMP_RATE_PER_HOUR * empHour;
         System.out.println("Employee Daily Wage : " + empWage);
     }
