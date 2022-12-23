e994182cd58b529e649a47c218ce3007a4e03eec
vishalchandra736 <vishalchandra736@gmail.com>
2022-12-23 17:59:02 +0530

    Added Part-Time Employee & Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,11 +3,14 @@ package com.blz.employeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         int IS_Full_Time = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHour, empWage;
-        int empCheck = (int)(Math.floor(Math.random() * 10 % 2));
+        int empCheck = (int)(Math.floor(Math.random() * 10 % 3));
         if (empCheck == IS_Full_Time)
             empHour = 8;
+        else if (empCheck == IS_PART_TIME)
+            empHour = 4;
         else
             empHour = 0;
         empWage = EMP_RATE_PER_HOUR * empHour;
