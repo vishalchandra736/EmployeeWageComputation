d748e531044fb3fcd2aab223a9f8ebe5e579d10e
vishalchandra736 <vishalchandra736@gmail.com>
2022-12-23 18:43:51 +0530

    Calculate Monthly Total Employee Wages

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,25 +2,24 @@ package com.blz.employeewagecomputation;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-//        int IS_FULL_TIME = 1;
-//        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
-        int empHour, empWage;
-        int empCheck = (int)(Math.floor(Math.random() * 10 % 3));
-        switch (empCheck) {
-            case 1 :
-                System.out.println("Employee was work  Full-Time");
-                empHour = 8;
-                break;
-            case 2 :
-                System.out.println("Employee was work Part-Time");
-                empHour = 4;
-                break;
-            default :
-                System.out.println("Employee was Absent");
-                empHour = 0;
+        int NUM__OF_WORKING_DAYS = 20;
+        int empHour, empWage, monthEmpWages = 0;
+        for(int i=0; i<NUM__OF_WORKING_DAYS; i++) {
+            int empCheck = (int)(Math.floor(Math.random() * 10 % 3));
+            switch (empCheck) {
+                case 1 :
+                    empHour = 8;
+                    break;
+                case 2 :
+                    empHour = 4;
+                    break;
+                default :
+                    empHour = 0;
+            }
+            empWage = EMP_RATE_PER_HOUR * empHour;
+            monthEmpWages += empWage;
         }
-        empWage = EMP_RATE_PER_HOUR * empHour;
-        System.out.println("Employee Daily Wage : " + empWage);
+        System.out.println("Monthly Total Employee Wages : " + monthEmpWages);
     }
 }
