8b59a65737ace5fa784dd0113e15e51b9817f3b2
vishalchandra736 <vishalchandra736@gmail.com>
2022-12-23 19:13:58 +0530

    Calculate Wages till Total Working Hours or Days in reached a month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,23 +3,26 @@ package com.blz.employeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         int EMP_RATE_PER_HOUR = 20;
-        int NUM__OF_WORKING_DAYS = 20;
-        int empHour, empWage, monthEmpWages = 0;
-        for(int i=0; i<NUM__OF_WORKING_DAYS; i++) {
-            int empCheck = (int)(Math.floor(Math.random() * 10 % 3));
+        int NUM_OF_WORKING_DAYS = 20;
+        int MAX_HOURS_IN_MONTH = 100;
+        int empHour = 0, monthEmpWages = 0, totalWorkingDays = 1, totalEmpHours = 0;
+        while( totalWorkingDays <= NUM_OF_WORKING_DAYS && totalEmpHours <= MAX_HOURS_IN_MONTH) {
+            int empCheck = (int) (Math.floor(Math.random() * 10 % 3));
             switch (empCheck) {
-                case 1 :
+                case 1:
                     empHour = 8;
                     break;
-                case 2 :
+                case 2:
                     empHour = 4;
                     break;
-                default :
+                default:
                     empHour = 0;
             }
-            empWage = EMP_RATE_PER_HOUR * empHour;
-            monthEmpWages += empWage;
+            totalEmpHours += empHour;
+            System.out.println("Day : " + totalWorkingDays + " and Employee Hour : " + empHour);
+            totalWorkingDays++;
         }
+        monthEmpWages = EMP_RATE_PER_HOUR * totalEmpHours;
         System.out.println("Monthly Total Employee Wages : " + monthEmpWages);
     }
 }
