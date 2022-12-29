a558f849b8d154290df1457c957a3632d00669fd
vishalchandra736 <vishalchandra736@gmail.com>
2022-12-30 00:46:40 +0530

    Added : Use the class method to compute Employee Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,6 +1,19 @@
 package com.blz.employeewagecomputation;
 
 public class EmployeeWageComputation {
+    public static int checkWorkingHour() {
+        int empHour;
+        int empCheck = (int) (Math.floor(Math.random() * 10 % 3));
+        switch (empCheck) {
+            case 1:
+                return empHour = 8;
+            case 2:
+                return empHour = 4;
+            default:
+                return empHour = 0;
+        }
+    }
+
     public static void main(String[] args) {
         int EMP_RATE_PER_HOUR = 20;
         int NUM_OF_WORKING_DAYS = 20;
@@ -8,17 +21,7 @@ public class EmployeeWageComputation {
         int empHour = 0, monthEmpWages = 0, totalWorkingDays = 1, totalEmpHours = 0;
         for(int i=1; i<=NUM_OF_WORKING_DAYS && totalEmpHours <= MAX_HOURS_IN_MONTH; i++) {
         //while( totalWorkingDays <= NUM_OF_WORKING_DAYS && totalEmpHours <= MAX_HOURS_IN_MONTH) {
-            int empCheck = (int) (Math.floor(Math.random() * 10 % 3));
-            switch (empCheck) {
-                case 1:
-                    empHour = 8;
-                    break;
-                case 2:
-                    empHour = 4;
-                    break;
-                default:
-                    empHour = 0;
-            }
+            empHour = checkWorkingHour();
             totalEmpHours += empHour;
             System.out.println("Day : " + totalWorkingDays + " and Employee Hour : " + empHour);
             totalWorkingDays++;
