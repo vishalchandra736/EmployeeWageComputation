9295c7395785a7366dea73ffadc8a0c16e8e0e52
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-30 15:52:00 +0530

    Added : Compute wage for multiple companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,32 +1,72 @@
 package com.blz.employeewagecomputation;
 
+import java.util.Scanner;
+
 public class EmployeeWageComputation {
-    public static int checkWorkingHour() {
-        int empHour;
-        int empCheck = (int) (Math.floor(Math.random() * 10 % 3));
-        switch (empCheck) {
-            case 1:
-                return empHour = 8;
-            case 2:
-                return empHour = 4;
-            default:
-                return empHour = 0;
-        }
-    }
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
     public static void main(String[] args) {
-        int EMP_RATE_PER_HOUR = 20;
-        int NUM_OF_WORKING_DAYS = 20;
-        int MAX_HOURS_IN_MONTH = 100;
-        int empHour = 0, monthEmpWages = 0, totalWorkingDays = 1, totalEmpHours = 0;
-        for(int i=1; i<=NUM_OF_WORKING_DAYS && totalEmpHours <= MAX_HOURS_IN_MONTH; i++) {
-        //while( totalWorkingDays <= NUM_OF_WORKING_DAYS && totalEmpHours <= MAX_HOURS_IN_MONTH) {
-            empHour = checkWorkingHour();
-            totalEmpHours += empHour;
-            System.out.println("Day : " + totalWorkingDays + " and Employee Hour : " + empHour);
-            totalWorkingDays++;
+
+        computeEmpWage("OnePlus", 50, 15, 200);
+        computeEmpWage("Amazon", 80, 20, 120);
+        computeEmpWage("Meta", 60, 20, 100);
+        computeEmpWage("Oracle", 90, 18, 220);
+        computeEmpWage("Infosys", 100, 20, 250);
+    }
+
+    public static void computeEmpWage(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
+
+        System.out.println(" 1.OnePlus \n 2.Amazon \n 3.Meta \n 4.Oracle \n 5.Infosys \n Choose a company : ");
+        Scanner scanner = new Scanner(System.in);
+
+        int choose = scanner.nextInt();
+
+        if(choose <= 5 && choose > 0)
+        {
+
+            for (totalWorkingDays = 1; totalEmpHrs <= MAX_HRS_IN_MONTH &&
+                    totalWorkingDays <= NUM_OF_WORKING_DAYS; totalWorkingDays++) {
+                int empCheck = (int) ((Math.random() * 10) % 3);
+
+                switch(empCheck) {
+
+                    case IS_FULL_TIME :
+                        empHrs = 8;
+                        System.out.println("Employee is present : Full Time");
+                        break;
+
+                    case IS_PART_TIME :
+                        empHrs = 4;
+                        System.out.println("Employee is present : Part Time");
+                        break;
+
+                    default :
+                        empHrs = 0;
+                        System.out.println("Employee is absent");
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpHrs += empHrs;
+                totalEmpWage += empWage;
+                System.out.println("Day " + totalWorkingDays + " Employee hours : " + empHrs +"	Wage $" + empWage);
+            }
+
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            System.out.println("\n Employee Monthly Wage : $" + totalEmpWage);
+            System.out.println();
         }
-        monthEmpWages = EMP_RATE_PER_HOUR * totalEmpHours;
-        System.out.println("Monthly Total Employee Wages : " + monthEmpWages);
+        else
+        {
+            System.out.println("Invalid Input ");
+        }
     }
 }
